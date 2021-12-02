8e5d8452d14f7539fa65b104e697549795184447
Scott Hammer <shammer@amerisure.com>
2021-12-02 14:07:08 -0500

    SH Resetting to baseline Android project with anticipated config/dependencies

--- app/src/main/java/com/shammer/udemytdd/MainActivity.kt
+++ app/src/main/java/com/shammer/udemytdd/MainActivity.kt
@@ -7,9 +7,6 @@ import com.shammer.udemytdd.`coroutine-example`.Engine
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val engine = Engine(500, 195)
-        val car = Car(6.0, engine)
-        car.start()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
